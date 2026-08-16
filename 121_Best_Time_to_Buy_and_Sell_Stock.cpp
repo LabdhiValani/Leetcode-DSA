36c88fca294ff69f7209d8031fb8a7ffc24890f1
LabdhiValani <labdhivalani28@gmail.com>
2026-08-16 23:59:12 +0530

    Best_Time_to_Buy_and_Sell_Stock Solution

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice=prices[0];
+        int maxprofit=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<minPrice){
+                minPrice=prices[i];
+            }
+            int profit=prices[i]-minPrice;
+            if(profit>maxprofit)
+                maxprofit=profit;
+        }
+        return maxprofit;
+    }
+};
